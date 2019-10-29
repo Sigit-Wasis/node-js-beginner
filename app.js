9e9da0a96fb0ec72136ef1770db03dadd0d32aed
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2019-10-29 10:05:01 +0700

    Mengirim Data Ke View

--- app.js
+++ app.js
@@ -2,68 +2,105 @@
  Basic Webserver
 ===============*/
 
-const http = require('http');
+// const http = require('http');
  
-http.createServer(function(req,res){
-  res.writeHead(200,{
-    "Content-Type" : "text/html"
-  });
-  res.end("Hello World");
-}).listen(8000);
+// http.createServer(function(req,res){
+//   res.writeHead(200,{
+//     "Content-Type" : "text/html"
+//   });
+//   res.end("Hello World");
+// }).listen(8000);
  
-console.log('Server is running at port 8000');
+// console.log('Server is running at port 8000');
 
 
 /*====================
  Basic Routing Express
 ====================*/
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-// route untuk halaman home
-app.get('/', (req, res) => {
-	res.send('Welcome To Express');
-})
+// // route untuk halaman home
+// app.get('/', (req, res) => {
+// 	res.send('Welcome To Express');
+// })
 
-// route untuk halaman about
-app.get('/about', (req, res) => {
-	res.send('This is about page');
-})
+// // route untuk halaman about
+// app.get('/about', (req, res) => {
+// 	res.send('This is about page');
+// })
 
-app.listen(8000, () => {
-	console.log('Server Is running at port 8000');
-})
+// app.listen(8000, () => {
+// 	console.log('Server Is running at port 8000');
+// })
 
 /* =================
 Serving Static Files
 ==================*/
 
 //use path module
+// const path = require('path');
+// //use express module
+// const express = require('express');
+// //use hbs view engine
+// const hbs = require('hbs');
+// const app = express();
+ 
+// //set dynamic views file
+// app.set('views',path.join(__dirname,'views'));
+// //set view engine
+// app.set('view engine', 'hbs');
+// //set public folder as static folder for static file
+// app.use(express.static('public'));
+// //route untuk halaman home
+// app.get('/',(req, res) => {
+//   //render file index.hbs
+//   res.render('index');
+// });
+ 
+// //route untuk halaman about
+// app.get('/about',(req, res) => {
+//   res.send('This is about page');
+// });
+ 
+// app.listen(8000, () => {
+//   console.log('Server is running at port 8000');
+// });
+
+/*===================
+Mengirim Data Ke View
+===================*/
+
+// use path module
 const path = require('path');
-//use express module
+// use express module
 const express = require('express');
-//use hbs view engine
+// use hbs view engine
 const hbs = require('hbs');
 const app = express();
- 
-//set dynamic views file
-app.set('views',path.join(__dirname,'views'));
-//set view engine
+
+// set views file
+app.set('views', path.join(__dirname, 'views'));
+// set view engine
 app.set('view engine', 'hbs');
-//set public folder as static folder for static file
+// set public folder as static folder for static file
 app.use(express.static('public'));
-//route untuk halaman home
+// route untuk halaman home
 app.get('/',(req, res) => {
-  //render file index.hbs
-  res.render('index');
+	// render file index.hbs
+	res.render('index', {
+		name: "Sigit wasis subekti"
+	});
 });
- 
-//route untuk halaman about
-app.get('/about',(req, res) => {
-  res.send('This is about page');
+// route untuk halaman home dengan parameter name
+app.get('/:name', (req, res) => {
+	// render file index.hbs
+	res.render('index', {
+		name: req.params.home
+	});
 });
- 
+
 app.listen(8000, () => {
-  console.log('Server is running at port 8000');
+	console.log('Server is running at port 8080');
 });
