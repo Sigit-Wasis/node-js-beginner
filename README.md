08ce5b6c87b3171de529ccc1926a80c02785266f
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2019-10-29 10:49:02 +0700

    Menghandle Post Request Body

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# node-js-beginner
+<p align="center">
+<a href="https://embuncode.blogspot.com/" class="btn btn-success">Embun Code</a>
+</p>
+
+## Node-js-beginner
+
+Nodejs adalah sebuah platform untuk mengeksekusi program Javascript di luar browser.
+Nodejs menggunakan runtime engine bernama V8, yang merupakan javascript runtime engine dari Google Chrome.
 Node js beginner tutorial about basic web server, express framework, Basic Routing Express &amp; Serving Static Files
+
+## Reference
+
+- [Pengenalan Node Js](https://www.petanikode.com/nodejs-pemula/).
+- [Tutorial Node Js](http://mfikri.com/artikel/tutorial-nodejs).
--- app.js
+++ app.js
@@ -72,18 +72,59 @@ Serving Static Files
 Mengirim Data Ke View
 ===================*/
 
+// // use path module
+// const path = require('path');
+// // use express module
+// const express = require('express');
+// // use hbs view engine
+// const hbs = require('hbs');
+// const app = express();
+
+// // set views file
+// app.set('views', path.join(__dirname, 'views'));
+// // set view engine
+// app.set('view engine', 'hbs');
+// // set public folder as static folder for static file
+// app.use(express.static('public'));
+// // route untuk halaman home
+// app.get('/',(req, res) => {
+// 	// render file index.hbs
+// 	res.render('index', {
+// 		name: "Sigit wasis subekti"
+// 	});
+// });
+// // route untuk halaman home dengan parameter name
+// app.get('/:name', (req, res) => {
+// 	// render file index.hbs
+// 	res.render('index', {
+// 		name: req.params.home
+// 	});
+// });
+
+// app.listen(8000, () => {
+// 	console.log('Server is running at port 8080');
+// });
+
+/*==========================
+Menghandle POST request Body
+==========================*/
+
 // use path module
 const path = require('path');
 // use express module
 const express = require('express');
 // use hbs view engine
 const hbs = require('hbs');
+//use bodyParser middleware
+const bodyParser = require('body-parser');
 const app = express();
 
 // set views file
 app.set('views', path.join(__dirname, 'views'));
 // set view engine
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 // set public folder as static folder for static file
 app.use(express.static('public'));
 // route untuk halaman home
@@ -93,11 +134,18 @@ app.get('/',(req, res) => {
 		name: "Sigit wasis subekti"
 	});
 });
-// route untuk halaman home dengan parameter name
-app.get('/:name', (req, res) => {
-	// render file index.hbs
+// route untuk menampilkan form
+app.get('/post', (req, res) => {
+	// render file form.hbs
+	res.render('form');
+});
+
+// route untuk submit form
+app.post('/post', (req, res) => {
+	// render file form.hbs
 	res.render('index', {
-		name: req.params.home
+		// ambil value dari textname
+		name : req.body.textname
 	});
 });
 
